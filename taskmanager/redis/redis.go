fc6f1503c40e1031cab24afeedcce085596a6fda
wineandchord <guoqizhou19@gmail.com>
2025-04-16 21:32:28 +0800

    a2a-go: change license and headers (#9)

--- taskmanager/redis/redis.go
+++ taskmanager/redis/redis.go
@@ -1,11 +1,8 @@
-// Tencent is pleased to support the open source community by making tRPC available.
+// Tencent is pleased to support the open source community by making a2a-go available.
 //
-// Copyright (C) 2025 THL A29 Limited, a Tencent company.
-// All rights reserved.
+// Copyright (C) 2025 THL A29 Limited, a Tencent company.  All rights reserved.
 //
-// If you have downloaded a copy of the tRPC source code from Tencent,
-// please note that tRPC source code is licensed under the  Apache 2.0 License,
-// A copy of the Apache 2.0 License is included in this file.
+// a2a-go is licensed under the Apache License Version 2.0.
 
 // Package redis provides a Redis-based implementation of the A2A TaskManager interface.
 package redis
